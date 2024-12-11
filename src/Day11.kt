05af50d04b47aa90d0a78bd62372371f2995e577
Benjamin Mohr <benjamin.mohr@outlook.com>
2024-12-11 08:52:25 +0100

    add solutions for day11

---
+++ src/Day11.kt
@@ -0,0 +1,51 @@
+package day11
+
+import readInput
+import shouldBe
+
+fun main() {
+    solve(readInput("day11_test"), 6) shouldBe 22
+    solve(readInput("day11_test"), 25) shouldBe 55312
+    solve(readInput("day11"), 25) shouldBe 211306
+    solve(readInput("day11"), 75) shouldBe 250783680217283
+}
+
+fun solve(input: String, blinkCount: Int): Long {
+    val stones = input.split(" ").map(String::toLong)
+    val solver = Solver()
+    return stones.sumOf { stone ->
+        solver.blink(stone, blinkCount)
+    }
+}
+
+class Solver {
+    private val cachedResults = mutableMapOf<Long, MutableMap<Int, Long>>()
+
+    private fun getOrPut(stone: Long, blinkCount: Int, calc: () -> Long): Long {
+        return cachedResults.getOrPut(stone) {
+            mutableMapOf(blinkCount to calc())
+        }.getOrPut(blinkCount) {
+            calc()
+        }
+    }
+
+    fun blink(stone: Long, blinkCount: Int): Long {
+        return getOrPut(stone, blinkCount) {
+            calculateBlink(stone, blinkCount)
+        }
+    }
+
+    private fun calculateBlink(stone: Long, blinkCount: Int): Long {
+        if (blinkCount == 0) return 1
+        return when {
+            stone == 0L -> blink(1L, blinkCount - 1)
+            stone.toString().length % 2 == 0 -> {
+                val text = stone.toString()
+                blink(text.substring(0, text.length / 2).toLong(), blinkCount - 1) +
+                        blink(text.substring(text.length / 2).toLong(), blinkCount - 1)
+            }
+
+            else -> blink(stone * 2024, blinkCount - 1)
+        }
+    }
+}
